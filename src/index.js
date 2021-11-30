e006df304fb175e38f733aa7aec0d29591fee3cf
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-11-29 23:36:43 -0300

    adiciona funcionalidade que valida se uma conta ja foi cadastrada pelo cpf

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ app.use(express.json());
 const accountList = new Set();
 
 app.post("/accounts", (request, response) => {
+  for (const account of accountList) {
+    if (account.cpf === request.body.cpf) {
+      return response.status(400).json({
+        error: "account already exists!",
+      })
+    }
+  }
+
   const account = {
     id: randomUUID(),
     name: request.body.name,
