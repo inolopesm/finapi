76bffebe06275c7f7556ab59578b846499149e65
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-11-29 23:31:30 -0300

    adiciona funcionalidade de cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const { randomUUID } = require("crypto")
 
 const app = express();
+app.use(express.json());
+
+const accountList = new Set();
+
+app.post("/accounts", (request, response) => {
+  const account = {
+    id: randomUUID(),
+    name: request.body.name,
+    cpf: request.body.cpf,
+    statement: [],
+  };
+
+  accountList.add(account);
+  return response.json(account);
+});
 
 app.listen(3333);
