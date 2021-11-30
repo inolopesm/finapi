000d8af47c570b62513c99bc601a970c3e8593e7
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-11-29 23:58:59 -0300

    adiciona funcionalidade de criar um deposito na conta

--- src/index.js
+++ src/index.js
@@ -7,26 +7,20 @@ app.use(express.json());
 const accountList = new Set();
 
 function verifyIfAccountExistsByCPF(request, response, next) {
-  const { accountCpf } = request.params
-
   for (const account of accountList) {
-    if (account.cpf === accountCpf) {
+    if (account.cpf === request.params.accountCpf) {
       request.account = account
       return next();
     }
   }
 
-  return response.status(400).json({
-    error: "account not found",
-  });
+  return response.status(400).json({ error: "account not found" });
 }
 
 app.post("/accounts", (request, response) => {
   for (const account of accountList) {
     if (account.cpf === request.body.cpf) {
-      return response.status(400).json({
-        error: "account already exists!",
-      });
+      return response.status(400).json({ error: "account already exists!" });
     }
   }
 
@@ -34,7 +28,7 @@ app.post("/accounts", (request, response) => {
     id: randomUUID(),
     name: request.body.name,
     cpf: request.body.cpf,
-    statement: [],
+    statement: new Set(),
   };
 
   accountList.add(account);
@@ -45,7 +39,24 @@ app.get(
   "/accounts/:accountCpf/statement",
   verifyIfAccountExistsByCPF,
   (request, response) => {
-    return response.json(request.account.statement);
+    return response.json(Array.from(request.account.statement));
+  }
+);
+
+app.post(
+  "/accounts/:accountCpf/deposits",
+  verifyIfAccountExistsByCPF,
+  (request, response) => {
+    const statementOperation = {
+      id: randomUUID(),
+      type: "credit",
+      description: request.body.description,
+      amount: request.body.amount,
+      created_at: new Date(),
+    };
+
+    request.account.statement.add(statementOperation)
+    return response.json(statementOperation)
   }
 );
 
